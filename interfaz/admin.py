f1f169978cea2cadb925a4e114abf5eb608c22e2
NicolasAgustin <nickrak10@gmail.com>
2023-08-16 21:39:16 -0300

    Added custom admin class

--- interfaz/admin.py
+++ interfaz/admin.py
@@ -1,6 +1,15 @@
+from typing import Any
 from django.contrib import admin
 
+from interfaz.utils.enctyption_utils import encrypt_password
 # Register your models here.
 from interfaz.models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    def save_model(self, request: Any, obj: User, form: Any, change: Any) -> None:
+        obj.password = encrypt_password(obj.password)
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(User, UserAdmin)
