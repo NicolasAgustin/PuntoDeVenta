cf535dcc162503e878ad70aba8d5692e602dea98
NicolasAgustin <nickrak10@gmail.com>
2023-08-17 23:56:02 -0300

    Added password field

--- interfaz/forms.py
+++ interfaz/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Usuario", max_length=50)
-    password = forms.CharField(label="Contraseña", max_length=100)
+    password = forms.CharField(label="Contraseña", max_length=100, widget=forms.PasswordInput())
